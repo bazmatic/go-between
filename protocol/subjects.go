679a82a698739b34eadb2fc147a2a76b24a8b991
Barry Earsman <barry@earsman.com>
2019-07-18 15:21:32 +0200

    Capitalise entity names in subjects, as specified

--- protocol/subjects.go
+++ protocol/subjects.go
@@ -1,13 +1,13 @@
 package protocol
 
 // SubjectUserCreate - NATS subject string for creating a new user
-const SubjectUserCreate string = "user.create"
+const SubjectUserCreate string = "User.create"
 
 // SubjectUserCreateCompleted - NATS subject string for confirming creation of a new user
-const SubjectUserCreateCompleted string = "user.create.completed"
+const SubjectUserCreateCompleted string = "User.create.completed"
 
 // SubjectUserList - NATS subject string for listing users
-const SubjectUserList string = "user.list"
+const SubjectUserList string = "User.list"
 
 // SubjectUserListCompleted - NATS subject string containing a list of users
-const SubjectUserListCompleted string = "user.list.completed"
+const SubjectUserListCompleted string = "User.list.completed"
